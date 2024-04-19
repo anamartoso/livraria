ad161f95a6c9eec1a51fb72b84ffcebf541966e8
anamartoso <166146748+anamartoso@users.noreply.github.com>
2024-04-19 09:21:00 -0300

    Update js.js

--- js.js
+++ js.js
@@ -3,8 +3,8 @@ var id = 0;
 
 $.ajax(url)
     .done(function(xml){
-        $(xml).find("pirata").each(function(){
-            $("#cards").append('<div class="card"><a href="individual.html?id='+$(this).find("id").text()+'"><p class="procurado">Procurado</p> <img class="foto"src="'+ $(this).find("id").text() +'.jpg"><p class="vivo">Vivo ou Morto</p> <p class="nome">'+ $(this).find("nome").text() +'</p> <p>'+ $(this).find("alcunha").text()+'<\p> <p class="rec"> <img class="berrys" src="Berrys.png">'+ $(this).find("recompensa").text()+' </p><p> Status:'+ $(this).find("status").text()+'</a> </p><\div>');
+        $(xml).find("titulos").each(function(){
+            $("#cards").append('<div class="card"><a href="individual.html?id='+$(this).find("id").text()+'"><p class="procurado">Procurado</p> <img class="foto"src="'+ $(this).find("id").text() +'.jpg"><p class="vivo">Vivo ou Morto</p> <p class="nome">'+ $(this).find("nome").text() +'</p> <p>'+ $(this).find("alcunha").text()+'<\p> <p class="rec"> <img class="berrys" src="Berrys.png">'+ $(this).find("valores").text()+' </p><p> Status:'+ $(this).find("status").text()+'</a> </p><\div>');
         });
     })
     .fail(function(){
@@ -20,7 +20,7 @@ $.ajax(url)
 
     $.ajax(url)
     .done(function(xml){
-            $(xml).find("pirata").each(function(){
+            $(xml).find("titulos").each(function(){
                 var pos = parseInt($(this).find("id").text())
                 if(id == pos){
                 $("#individual").append('<div class="card"><p class="procurado">Procurado</p> <img class="foto"src="'+ $(this).find("id").text() +'.jpg"><p class="vivo">Vivo ou Morto</p> <p class="nome">'+ $(this).find("nome").text() +'</p> <p>'+ $(this).find("alcunha").text()+'<\p> <p class="rec"> <img class="berrys" src="Berrys.png">'+ $(this).find("recompensa").text()+' </p><p> Status:'+ $(this).find("status").text()+' </p><\div>');
