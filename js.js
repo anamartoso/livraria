480d39768fbd3761ddb7320326d3945186578321
anamartoso <166146748+anamartoso@users.noreply.github.com>
2024-05-01 12:22:14 -0300

    Update js.js

--- js.js
+++ js.js
@@ -1,33 +1,29 @@
-let url="dados.xml";
+let url = "dados.xml";
 var id = 0;
 
 $.ajax(url)
     .done(function(xml){
-        $(xml).find("titulos").each(function(){
-            $("#cards").append('<div class="card"><a href="individual.html?id='+$(this).find("id").text()+'"><p class="procurado">Procurado</p> <img class="foto"src="'+ $(this).find("id").text() +'.jpg"><p class="vivo">Vivo ou Morto</p> <p class="nome">'+ $(this).find("nome").text() +'</p> <p>'+ $(this).find("alcunha").text()+'<\p> <p class="rec"> <img class="berrys" src="Berrys.png">'+ $(this).find("valores").text()+' </p><p> Status:'+ $(this).find("status").text()+'</a> </p><\div>');
+        $(xml).find("livro").each(function(){
+            $("#cards").append('<div class="card"><a href="individual.html?id='+$(this).find("id").text()+'"><p class="titulo">'+ $(this).find("titulo").text() +'</p> <p class="autor">'+ $(this).find("autor").text() +'</p> <p class="genero">'+ $(this).find("genero").text()+'</p> <p class="ano">Ano:'+ $(this).find("ano").text()+'</p> <p class="status">'+ $(this).find("status").text()+'</p></a></div>');
         });
     })
     .fail(function(){
         alert("Ocorreu um erro na leitura do arquivo XML.");
-    })
+    });
 
-   
+var url_string = window.location.href;
+var url1 = new URL(url_string);
+var id = parseInt(url1.searchParams.get("id"));
 
-
-    var url_string = window.location.href;
-    var url1 = new URL(url_string);
-    var id = parseInt(url1.searchParams.get("id"));
-
-    $.ajax(url)
+$.ajax(url)
     .done(function(xml){
-            $(xml).find("titulos").each(function(){
-                var pos = parseInt($(this).find("id").text())
-                if(id == pos){
-                $("#individual").append('<div class="card"><p class="procurado">Procurado</p> <img class="foto"src="'+ $(this).find("id").text() +'.jpg"><p class="vivo">Vivo ou Morto</p> <p class="nome">'+ $(this).find("nome").text() +'</p> <p>'+ $(this).find("alcunha").text()+'<\p> <p class="rec"> <img class="berrys" src="Berrys.png">'+ $(this).find("recompensa").text()+' </p><p> Status:'+ $(this).find("status").text()+' </p><\div>');
-                }
-            });
-        
+        $(xml).find("livro").each(function(){
+            var pos = parseInt($(this).find("id").text());
+            if(id == pos){
+                $("#individual").append('<div class="card"><p class="titulo">'+ $(this).find("titulo").text() +'</p> <p class="autor">'+ $(this).find("autor").text() +'</p> <p class="genero">'+ $(this).find("genero").text()+'</p> <p class="ano">Ano:'+ $(this).find("ano").text()+'</p> <p class="status">'+ $(this).find("status").text()+'</p></div>');
+            }
+        });
     })
     .fail(function(){
         alert("Ocorreu um erro na leitura do arquivo XML.");
-    })
+    });
